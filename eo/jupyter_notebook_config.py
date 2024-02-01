f610c66fe5940382f0d0405fde08c06407a11ac8
bnjam <benjamin@bnjam.dev>
2024-02-01 10:10:21 -0800

    staging updates

--- eo/jupyter_notebook_config.py
+++ eo/jupyter_notebook_config.py
@@ -3,3 +3,4 @@ import os
 # Configure nb_conda_kernels to avoid registering jupyter kernels in our conda
 # environment again.
 c.CondaKernelSpecManager.env_filter = f'.*envs/{os.environ["CONDA_ENV"]}.*'
+c.Completer.use_jedi = False
